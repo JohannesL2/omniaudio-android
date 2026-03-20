2775a741042ca3ae98087a2a2b90005b03cac1fb
JohannesL2 <johannes.lindgren2002@gmail.com>
2026-03-20 09:05:20 +0100

    Created PlayerManager, this initiates media3 ExoPlayer and gives us functions to play and pause the music

---
+++ app/src/main/java/com/johannesl2/omniaudio/player/PlayerManager.kt
@@ -0,0 +1,25 @@
+package com.johannesl2.omniaudio.player
+
+import android.content.Context
+import androidx.media3.common.MediaItem
+import androidx.media3.exoplayer.ExoPlayer
+
+class PlayerManager(context: Context) {
+
+    private val player: ExoPlayer = ExoPlayer.Builder(context).build()
+
+    fun play(url: String) {
+        val mediaItem = MediaItem.fromUri(url)
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.play()
+    }
+
+    fun pause() {
+        player.pause()
+    }
+
+    fun release() {
+        player.release()
+    }
+}
